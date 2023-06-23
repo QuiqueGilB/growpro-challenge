3a55a26cc09cb645fc5be292166ac99fc55db15f
Enrique Gil <hello@enriquegil.me>
2023-06-23 17:19:46 +0200

    feat: Init project

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace QuiqueGilB\GrowPro;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
