f0ad7a007571158a819fe089ff74ccfdcd2304ab
Enrique Gil <hello@enriquegil.me>
2023-06-23 21:21:39 +0200

    fix: namespace error

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use QuiquegilB\GrowPro\SharedContext\SymfonyModule\Infrastructure\Symfony\Kernel\Kernel;
+use QuiqueGilB\GrowPro\SharedContext\SymfonyModule\Infrastructure\Symfony\Kernel\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
