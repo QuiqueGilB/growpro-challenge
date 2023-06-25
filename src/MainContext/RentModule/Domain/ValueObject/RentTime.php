16a1100e66e44c6fa1f76a51135fd2f774c6e705
Enrique Gil <hello@enriquegil.me>
2023-06-25 11:52:40 +0200

    feat: RentTime

---
+++ src/MainContext/RentModule/Domain/ValueObject/RentTime.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuiqueGilB\GrowPro\MainContext\RentModule\Domain\ValueObject;
+
+use QuiqueGilB\GrowPro\SharedContext\SharedModule\Domain\ValueObject\DurationValueObject;
+
+final readonly class RentTime extends DurationValueObject
+{
+
+}
