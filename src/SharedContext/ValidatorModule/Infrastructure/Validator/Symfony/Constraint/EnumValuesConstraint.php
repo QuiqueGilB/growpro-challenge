d9439e6b0ee21f086c418598f747deea5cfc2369
Enrique Gil <hello@enriquegil.me>
2023-06-28 09:48:43 +0200

    fix: error constraint for xml mapping

--- src/SharedContext/ValidatorModule/Infrastructure/Validator/Symfony/Constraint/EnumValuesConstraint.php
+++ src/SharedContext/ValidatorModule/Infrastructure/Validator/Symfony/Constraint/EnumValuesConstraint.php
@@ -9,10 +9,21 @@ use Symfony\Component\Validator\Constraints\Choice;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 final class EnumValuesConstraint extends Choice
 {
-    #[AsNamedArgument]
-    public function __construct(string $className) {
-        parent::__construct(func_get_args());
-        $this->choices = array_map(static fn($case) => $case->value, $className::cases());
+    protected string $className;
+
+    protected function normalizeOptions(mixed $options): array
+    {
+        $options = parent::normalizeOptions($options);
+
+        $this->className = $options['className'];
+        $this->choices = array_map(static fn($case) => $case->value, $this->className::cases());
+
+        return $options;
+    }
+
+    public function className(): string
+    {
+        return $this->className;
     }
 
     public static function getErrorName(string $errorCode): string
