f801e97e988e6de1196a4ed2b04b1da9f4293e8a
Enrique Gil <hello@enriquegil.me>
2023-06-28 11:40:27 +0200

    refactor: field name

--- src/MainContext/RentModule/Application/Request/Bike/PostBikeRequest.php
+++ src/MainContext/RentModule/Application/Request/Bike/PostBikeRequest.php
@@ -7,7 +7,7 @@ namespace QuiqueGilB\GrowPro\MainContext\RentModule\Application\Request\Bike;
 final readonly class PostBikeRequest
 {
     public function __construct(
-        public string $id,
+        public string $bikeId,
         public string $type,
         public string $plan,
     ) {
